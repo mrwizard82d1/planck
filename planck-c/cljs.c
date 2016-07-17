f9b5330ab239c52d0b3cfc8471a66e713783916d
Mike Fikes <mike@fikesfarm.com>
2016-07-17 15:41:38 -0400

    Implemente PLANCK_IS_DIRECTORY

--- planck-c/cljs.c
+++ planck-c/cljs.c
@@ -354,6 +354,8 @@ void *cljs_do_engine_init(void *data) {
 	register_global_function(ctx, "PLANCK_FILE_OUTPUT_STREAM_WRITE", function_file_output_stream_write);
 	register_global_function(ctx, "PLANCK_FILE_OUTPUT_STREAM_CLOSE", function_file_output_stream_close);
 
+	register_global_function(ctx, "PLANCK_IS_DIRECTORY", function_is_directory);
+
 	register_global_function(ctx, "PLANCK_FSTAT", function_fstat);
 
 	register_global_function(ctx, "PLANCK_REQUEST", function_http_request);
--- planck-c/functions.c
+++ planck-c/functions.c
@@ -639,6 +639,31 @@ JSValueRef function_file_output_stream_close(JSContextRef ctx, JSObjectRef funct
 	return JSValueMakeNull(ctx);
 }
 
+JSValueRef function_is_directory(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
+											 size_t argc, const JSValueRef args[], JSValueRef* exception) {
+	if (argc == 1
+		&& JSValueGetType(ctx, args[0]) == kJSTypeString) {
+
+		char *path = value_to_c_string(ctx, args[0]);
+
+		bool is_directory = false;
+
+		struct stat file_stat;
+
+		int retval = lstat(path, &file_stat);
+
+		free(path);
+
+		if (retval == 0) {
+			is_directory = S_ISDIR(file_stat.st_mode);
+		}
+
+		return JSValueMakeBoolean(ctx, is_directory);
+
+	}
+	return JSValueMakeNull(ctx);
+}
+
 JSValueRef function_fstat(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject,
 									  size_t argc, const JSValueRef args[], JSValueRef* exception) {
 	if (argc == 1
